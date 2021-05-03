371ba9de0bf03a99a51e418c03d26ec68c9c605c
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-03 13:02:06 +0200

    finish contact us page 3

---
+++ ContactUs3.php
@@ -0,0 +1,249 @@
+<!-- START:: INCLUDING THE HEADER -->
+<?php include('layouts/TheHeader.php')?>
+<!-- END:: INCLUDING THE HEADER -->
+
+<!-- START:: HEDAER IMAGE SECTION -->
+<div class="header_img_wraper" style="background: url('assets/media/pic6.jpeg');">
+  <!-- <img src="assets/media/bg.jpg" alt="Intro Img">   -->
+
+  <div class="overlay">
+    <div class="intro_text_wraper">
+      <h1> Get In Touch <span> With Us</span> </h1>
+    </div>
+  </div>
+</div>
+<!-- END:: HEDAER IMAGE SECTION -->
+
+<!-- START:: GET IN TOUCH SECTION -->
+<div class="get_in_touch">
+  <div class="container">
+    <div class="row justify-content-center my-4 social">
+      <div class="col-3 col-md-1 text-center">
+        <a href="#" target="_blanck">
+          <img src="assets/media/icons/social/facebook.png" alt="">
+        </a>
+      </div>
+
+      <div class="col-3 col-md-1 text-center">
+        <a href="#" target="_blanck">
+          <img src="assets/media/icons/social/instagram.png" alt="">
+        </a>
+      </div>
+
+      <div class="col-3 col-md-1 text-center">
+        <a href="#" target="_blanck">
+          <img src="assets/media/icons/social/youtube.png" alt="">
+        </a>
+      </div>
+
+      <div class="col-3 col-md-1 text-center">
+        <a href="#" target="_blanck">
+          <img src="assets/media/icons/social/twitter.png" alt="">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: GET IN TOUCH SECTION -->
+
+<!-- START:: CONTACT INFO SECTION -->
+<div class="contact_info my-5">
+  <div class="container">
+    <div class="row justify-content-center justify-content-md-between">
+      <div class="col-6 col-md-4">
+        <div class="card">
+          <span>
+            <i class="fas fa-map-marker-alt fa-3x"></i>
+          </span>
+          <h4> Address </h4>
+          <h5 class="text-center"> The Address Will Go Here </h5>
+        </div>
+      </div>
+
+      <div class="col-6 col-md-4">
+        <div class="card">
+          <span>
+            <i class="fas fa-phone-alt fa-3x"></i>
+          </span>
+          <h4> Phones </h4>
+          <h5 class="text-center"> 01000000001 - 01000000002 </h5>
+        </div>
+      </div>
+
+      <div class="col-6 col-md-4">
+        <div class="card">
+          <span>
+            <i class="fas fa-at fa-3x"></i>
+          </span>
+          <h4> Email </h4>
+          <h5 class="text-center"> info@localhost.com </h5>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: CONTACT INFO SECTION -->
+
+<!-- START:: BRACHES SCTION -->
+<div class="branches_sec">
+  <div class="container">
+    <div class="row justify-content-center">
+
+      <div class="col-12 col-md-6 p-3">
+        <div class="map_info_wraper">
+          <h3> Branch Name 1 </h3>
+          <div class="iframe_waper">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110532.89549083744!2d31.30329406893522!3d30.032468600716175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c1380cba7ef%3A0xd541260e9e06978d!2sNasr%20City%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1601175174423!5m2!1sen!2seg" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+          
+          <div class="map_text">
+            <ul class="list-unstyled">
+              <li> 
+                <span> <i class="fas fa-map-marker-alt"></i> </span>
+                Branch Address Will Go Here
+              </li>
+            
+              <li> 
+                <span> <i class="fas fa-mobile-alt"></i> </span>
+                01000000001 - 01000000002
+              </li>
+            
+              <li> 
+                <span> <i class="far fa-envelope"></i> </span>
+                info@localhost.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 p-3">
+        <div class="map_info_wraper">
+          <h3> Branch Name 2 </h3>
+          <div class="iframe_waper">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110532.89549083744!2d31.30329406893522!3d30.032468600716175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c1380cba7ef%3A0xd541260e9e06978d!2sNasr%20City%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1601175174423!5m2!1sen!2seg" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+          
+          <div class="map_text">
+            <ul class="list-unstyled">
+              <li> 
+                <span> <i class="fas fa-map-marker-alt"></i> </span>
+                Branch Address Will Go Here
+              </li>
+            
+              <li> 
+                <span> <i class="fas fa-mobile-alt"></i> </span>
+                01000000001 - 01000000002
+              </li>
+            
+              <li> 
+                <span> <i class="far fa-envelope"></i> </span>
+                info@localhost.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 p-3">
+        <div class="map_info_wraper">
+          <h3> Branch Name 3 </h3>
+          <div class="iframe_waper">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110532.89549083744!2d31.30329406893522!3d30.032468600716175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c1380cba7ef%3A0xd541260e9e06978d!2sNasr%20City%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1601175174423!5m2!1sen!2seg" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+          
+          <div class="map_text">
+            <ul class="list-unstyled">
+              <li> 
+                <span> <i class="fas fa-map-marker-alt"></i> </span>
+                Branch Address Will Go Here
+              </li>
+            
+              <li> 
+                <span> <i class="fas fa-mobile-alt"></i> </span>
+                01000000001 - 01000000002
+              </li>
+            
+              <li> 
+                <span> <i class="far fa-envelope"></i> </span>
+                info@localhost.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 p-3">
+        <div class="map_info_wraper">
+          <h3> Branch Name 4 </h3>
+          <div class="iframe_waper">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110532.89549083744!2d31.30329406893522!3d30.032468600716175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c1380cba7ef%3A0xd541260e9e06978d!2sNasr%20City%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1601175174423!5m2!1sen!2seg" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+          
+          <div class="map_text">
+            <ul class="list-unstyled">
+              <li> 
+                <span> <i class="fas fa-map-marker-alt"></i> </span>
+                Branch Address Will Go Here
+              </li>
+            
+              <li> 
+                <span> <i class="fas fa-mobile-alt"></i> </span>
+                01000000001 - 01000000002
+              </li>
+            
+              <li> 
+                <span> <i class="far fa-envelope"></i> </span>
+                info@localhost.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: BRACHES SCTION -->
+
+<!-- START:: HOW TO SHARE WITH US SECTION -->
+<div class="share">
+  <div class="container">
+    <div class="row justify-content-center">
+
+      <div class="col-12 col-md-8 form_wraper">
+        <div class="header_wraper justify-content-center">
+          <h2> Write Us A Message </h2>
+        </div>
+
+        <form>
+          <div class="row">
+            <div class="col-12 col-md-6 my-3 form-group">
+              <input type="text" class="form-control" placeholder="Your Name">
+            </div>
+
+            <div class="col-12 col-md-6 my-3 form-group">
+              <input type="email" class="form-control" placeholder="Email Address">
+            </div>
+
+            <div class="col-12 my-3 form-group">
+              <input type="tele" class="form-control" placeholder="Phone Number">
+            </div>
+
+            <div class="col-12 my-3 form-group">
+              <textarea placeholder="Write A Message...."></textarea>
+            </div>
+          </div>
+
+          <div class="btn_box">
+            <button> Send Message </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: HOW TO SHARE WITH US SECTION -->
+
+<!-- START:: INCLUDING THE FOOTER -->
+<?php include('layouts/TheFooter.php')?>
+<!-- END:: INCLUDING THE FOOTER -->
