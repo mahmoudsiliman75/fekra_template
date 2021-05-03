e3b0eca757ceab9bb7440068c625905c13abe12b
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-03 10:54:29 +0200

    finish nwes section

--- index.php
+++ index.php
@@ -929,6 +929,145 @@
 </div>
 <!-- END:: FAQS SECTION -->
 
+<!-- START:: NEWS SECTION -->
+<div class="news_sec">
+  <div class="container">
+    <div class="header_wraper">
+      <h2> News </h2>
+    </div>
+
+    <div class="sec_desc_wraper">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+      Culpa enim praesentium minima sunt, eveniet ratione velit 
+      similique omnis porro doloremque dolores? Aut accusantium 
+      possimus voluptates nobis ullam? Amet, ipsa est.
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-10 col-lg-4 my-3">
+        <a href="#">
+          <div class="article_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic1.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Article Title 1 </h4>
+              <h5> 1 - 5 - 2021 </h5>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-3">
+        <a href="#">
+          <div class="article_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic2.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Article Title 2 </h4>
+              <h5> 1 - 5 - 2021 </h5>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-3">
+        <a href="#">
+          <div class="article_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic3.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Article Title 3 </h4>
+              <h5> 1 - 5 - 2021 </h5>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-3">
+        <a href="#">
+          <div class="article_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic4.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Article Title 4 </h4>
+              <h5> 1 - 5 - 2021 </h5>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-3">
+        <a href="#">
+          <div class="article_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic5.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Article Title 5 </h4>
+              <h5> 1 - 5 - 2021 </h5>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: NEWS SECTION -->
+
 <!-- START:: INCLUDING THE FOOTER -->
 <?php include('./layouts/TheFooter.php')?>
 <!-- END:: INCLUDING THE FOOTER -->
