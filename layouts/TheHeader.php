8a47b34364d2b9505cc7715749ac1abab9a3587d
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-04 15:41:42 +0200

    add preloader

--- layouts/TheHeader.php
+++ layouts/TheHeader.php
@@ -242,7 +242,7 @@
 <!-- END:: SMALL SCREENS MENU -->
 
 <!-- START:: PRE LOADER -->
-<!-- <div class="pre_loader">
+<div class="pre_loader">
   <div class="spinner">
     <div class="rect1"></div>
     <div class="rect2"></div>
@@ -250,5 +250,5 @@
     <div class="rect4"></div>
     <div class="rect5"></div>
   </div>
-</div> -->
+</div>
 <!-- END:: PRE LOADER -->
