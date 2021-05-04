8676ed68dd9c8b9a3128b111fb0521a90bc20b52
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-04 15:39:34 +0200

    finish profile page

---
+++ MemberProfile.php
@@ -0,0 +1,53 @@
+<!-- START:: INCLUDING THE HEADER -->
+<?php include('layouts/ProfileHeader.php')?>
+<!-- END:: INCLUDING THE HEADER -->
+
+<!-- START:: PROFILE SECTION -->
+<div class="profile_sec">
+  <div class="container">
+
+    <div class="card_wraper">
+      <div class="card_img">
+        <img src="assets/media/team/member1.jpg" alt="">
+      </div>
+
+      <div class="card_body">
+        <h5> Member Name </h5>
+        <h6> Member Role </h6>
+        <h6> info@localhost.com </h6>
+
+        <ul class="social_links">
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-facebook-f fa-lg"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-linkedin-in fa-lg"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-twitter fa-lg"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-instagram fa-lg"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- END:: PROFILE SECTION -->
+
+<!-- START:: INCLUDING THE FOOTER -->
+<?php include('layouts/TheFooter.php')?>
+<!-- END:: INCLUDING THE FOOTER -->
---
+++ layouts/ProfileHeader.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- START:: INCLUDING FONTAWESOME STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/all.min.css">
+  <!-- END:: INCLUDING FONTAWESOME STYLE FILE -->
+
+  <!-- START:: INCLUDING  LIGHT BOX STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/lightbox.css">
+  <!-- END:: INCLUDING  LIGHT BOX STYLE FILE -->
+
+  <!-- START:: INCLUDING OWL CAROUSEL STYLE FILES -->
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+  <!-- END:: INCLUDING OWL CAROUSEL STYLE FILES -->
+
+  <!-- START:: INCLUDING BOOTSTRAP STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- END:: INCLUDING BOOTSTRAP STYLE FILE -->
+
+  <!-- START:: INCLUDING FLEX SLIDER STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/flexslider.css">
+  <!-- END:: INCLUDING FLEX SLIDER STYLE FILE -->
+
+  <!-- START:: INCLUDING BOOTSTRAP SELECT STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+  <!-- END:: INCLUDING BOOTSTRAP SELECT STYLE FILE -->
+
+  <!-- START:: INCLUDING WOW JS STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/animate.css">
+  <!-- END:: INCLUDING WOW JS STYLE FILE -->
+
+  <!-- START:: INCLUDING MAIN STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- <link rel="stylesheet" href="assets/css/ar-style.css"> -->
+  <!-- END:: INCLUDING MAIN STYLE FILE -->
+
+  <!-- START:: SET FAV ICON -->
+  <link rel="icon" href="assets/media/icons/logo/logo.png">
+  <!-- END:: SET FAV ICON -->
+
+  <title> Fekra </title>
+</head>
+<body>
+
+<!-- START:: LARGE SCREENS NAVBAR -->
+<nav class="profile_nav d-flex justify-content-between align-items-center">
+  <!-- START:: THE LOGO -->
+  <div class="site-logo">
+    <a href="index.php">
+      <img src="assets/media/icons/logo/logo.png" alt="The Logo">
+    </a>
+  </div>
+  <!-- END:: THE LOGO -->
+
+  <!-- START:: NAV LINKS -->
+  <div class="nav-links">
+    <ul class="list-unstyled d-flex align-items-center justify-content-center mb-0">
+      <li class="active"> 
+        <a href="index.php"> Home </a>
+      </li>
+
+      <li> 
+        <a href="javascript:;" class="dropdown_toggeler"> About Us </a>
+
+        <ul class="dropdown_menu">
+          <li> 
+            <a href="AboutUs1.php"> About Us 1 </a> 
+          </li>
+
+          <li> 
+            <a href="AboutUs2.php"> About Us 2 </a> 
+          </li>
+        </ul>
+      </li>
+
+      <li> 
+        <a href="javascript:;" class="dropdown_toggeler"> Services </a>
+
+        <ul class="dropdown_menu">
+          <li> 
+            <a href="AllServices1.php"> Services 1 </a> 
+          </li>
+
+          <li> 
+            <a href="AllServices2.php"> Services 2 </a> 
+          </li>
+
+          <li> 
+            <a href="SingleService1.php"> Single Service 1 </a> 
+          </li>
+
+          <li> 
+            <a href="SingleService2.php"> Single Service 1 </a>  
+          </li>
+        </ul>
+      </li>
+
+      <li> 
+        <a href="Faqs.php"> FAQs </a>
+      </li>
+
+      <li> 
+        <a href="Team.php"> Our Team </a>
+      </li>
+
+      <li> 
+        <a href="Portfolio.php"> Portfolio </a>
+      </li>
+
+      <li> 
+        <a href="javascript:;" class="dropdown_toggeler"> Products </a>
+
+        <ul class="dropdown_menu">
+          <li> 
+            <a href="AllProducts1.php"> All Products 1 </a> 
+          </li>
+
+          <li> 
+            <a href="AllProducts2.php"> All Products 2 </a> 
+          </li>
+
+          <li> 
+            <a href="SingleProduct1.php"> Single Product 1 </a> 
+          </li>
+
+          <li> 
+            <a href="SingleProduct2.php"> Single Product 2 </a> 
+          </li>
+        </ul>
+      </li>
+
+      <li> 
+        <a href="javascript:;" class="dropdown_toggeler"> Blog </a>
+
+        <ul class="dropdown_menu">
+          <li> 
+            <a href="SidebarBlog.php"> Blog With SideBar </a> 
+          </li>
+
+          <li> 
+            <a href="SidebarArticle.php"> Article With SideBar </a> 
+          </li>
+        </ul>
+      </li>
+
+      <li> 
+        <a href="javascript:;" class="dropdown_toggeler"> Contact Us </a>
+
+        <ul class="dropdown_menu">
+          <li> 
+            <a href="ContactUs1.php">Contact Us 1</a> 
+          </li>
+
+          <li> 
+            <a href="ContactUs2.php">Contact Us 2</a>  
+          </li>
+
+          <li> 
+            <a href="ContactUs3.php">Contact Us 3</a> 
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <!-- START:: NAV LINKS -->
+
+  <div class="menu_btns d-flex align-items-center">
+    <!-- START:: LANG BUTTON -->
+    <div class="lang-button mx-2">
+      <a href="#"> 
+        <span class="en"> E </span>
+        <span class="ar"> ع </span>
+        <span class="circle"></span>
+      </a>
+    </div>
+    <!-- END:: LANG BUTTON -->
+
+    <!-- START:: MENU BUTTON -->
+    <button id="open_menu" class="menu_btn mx-2">
+      <i class="fas fa-bars"></i>
+    </button>
+    <!-- END:: MENU BUTTON -->
+  </div>
+</nav>
+<!-- END:: LARGE SCREENS NAVBAR -->
+
+<!-- START:: SMALL SCREENS MENU -->
+<!-- <div class="small_menu">
+  <div class="btn_box">
+    <button id="colse_menu">
+      <i class="far fa-times-circle"></i>
+    </button>
+  </div>
+
+  <ul class="list-unstyled mb-0">
+    <li class="active"> 
+      <a href="index.php"> Home </a>
+    </li>
+
+    <li> 
+      <a href="AboutUs.php"> About Us </a>
+    </li>
+
+    <li> 
+      <a href="Faq.php"> FAQs </a>
+    </li>
+
+    <li> 
+      <a href="AllProducts.php"> Product </a>
+    </li>
+
+    <li> 
+      <a href="Quality.php"> Quality </a>
+    </li>
+
+    <li> 
+      <a href="Media.php"> Media </a>
+    </li>
+
+    <li> 
+      <a href="AllVideos.php"> Videos </a>
+    </li>
+
+    <li> 
+      <a href="Carrers.php"> Carrers </a>
+    </li>
+
+    <li> 
+      <a href="ContactUs.php"> Contact Us </a>
+    </li>
+
+    <li> 
+      <a href="UserProfile.php"> Profile </a>
+    </li>
+  </ul>
+</div> -->
+<!-- END:: SMALL SCREENS MENU -->
+
+<!-- START:: PRE LOADER -->
+<!-- <div class="pre_loader">
+  <div class="spinner">
+    <div class="rect1"></div>
+    <div class="rect2"></div>
+    <div class="rect3"></div>
+    <div class="rect4"></div>
+    <div class="rect5"></div>
+  </div>
+</div> -->
+<!-- END:: PRE LOADER -->
