b276e8ac7098a0982111c93091013c2231602eb3
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-04 12:55:07 +0200

    finish about us bage 2

---
+++ AboutUs2.php
@@ -0,0 +1,54 @@
+<!-- START:: INCLUDING THE HEADER -->
+<?php include('layouts/TheHeader.php')?>
+<!-- END:: INCLUDING THE HEADER -->
+
+<!-- START:: HEDAER IMAGE SECTION -->
+<div class="header_img_wraper" style="background: url('assets/media/pic5.jpeg');">
+
+  <div class="overlay">
+    <div class="intro_text_wraper">
+      <h1> Know More <span> About Us</span> </h1>
+    </div>
+  </div>
+</div>
+<!-- END:: HEDAER IMAGE SECTION -->
+
+<!-- START:: ABOUT US SECTION -->
+<div class="about_us_1">
+  <!-- START:: ABOUT TEXT SECTION -->
+  <div class="container">
+    <div class="header_wraper">
+      <h2> Who We Are </h2>
+    </div>
+
+    <div class="row justify-content-center mt-5">
+      <div class="col-12 col-md-5">
+        <div class="img_wraper">
+          <img src="assets/media/pic1.jpeg" alt="">
+        </div>
+      </div>
+
+      <div class="col-12 col-md-7">
+        <div class="sec_desc_wraper">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Voluptatem, doloremque amet? Itaque enim placeat harum 
+          distinctio, veritatis, odit odio temporibus recusandae 
+          consequatur doloremque, neque tempore excepturi. 
+          Autem sequi architecto unde. odit odio temporibus recusandae 
+          consequatur doloremque, neque tempore excepturi. 
+          Autem sequi architecto unde. consequatur doloremque, 
+          neque tempore excepturi. 
+          Autem sequi architecto unde. odit odio temporibus recusandae 
+          consequatur doloremque, neque tempore excepturi. 
+          Autem sequi architecto unde.
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END:: ABOUT TEXT SECTION -->
+</div>
+<!-- END:: ABOUT US SECTION -->
+
+<!-- START:: INCLUDING THE FOOTER -->
+<?php include('layouts/TheFooter.php')?>
+<!-- END:: INCLUDING THE FOOTER -->
