77b3310f9d550b4e375c44565bd5ae61af4f47f8
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-17 13:51:57 +0200

    finish modern index services section

--- index_modern.php
+++ index_modern.php
@@ -142,172 +142,162 @@
 <!-- END:: ABOUT US SECTION -->
 
 <!-- START:: SERVICES SECTION -->
-<div class="services_sec_one_page">
-  <div class="overlay">
-    <div class="section_upper_curve_wraper">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
-      </svg>
+<div class="services_sec_modern">
+  <div class="container overflow-hidden">
+    <div class="header_wraper">
+      <h2 class="wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.9s">
+        <span></span>
+        <span></span> 
+        Our Services 
+      </h2>
     </div>
 
-    <div class="container">
-      <div class="header_wraper">
-        <h2> Our Services </h2>
-      </div>
-
-      <div class="sec_desc_wraper">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Culpa enim praesentium minima sunt, eveniet ratione velit 
-        similique omnis porro doloremque dolores? Aut accusantium 
-        possimus voluptates nobis ullam? Amet, ipsa est.
-      </div>
-
-      <div class="row justify-content-center">
-        <div class="col-10 col-lg-4 my-3">
-          <a href="#">
-            <div class="service_card_wraper">
-              <div class="img_wraper">
-                <div class="overlay">
-                  <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <img src="assets/media/pic1.jpeg" alt="">
-              </div>
-
-              <div class="card_title_wraper">
-                <h4> Service 1 </h4>
-              </div>
-
-              <div class="card_text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
-                facilis. Quas ex provident quam, inventore consectetur 
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-10 col-lg-4 my-3">
-          <a href="#">
-            <div class="service_card_wraper">
-              <div class="img_wraper">
-                <div class="overlay">
-                  <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <img src="assets/media/pic2.jpeg" alt="">
-              </div>
-
-              <div class="card_title_wraper">
-                <h4> Service 2 </h4>
-              </div>
-
-              <div class="card_text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
-                facilis. Quas ex provident quam, inventore consectetur 
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-10 col-lg-4 my-3">
-          <a href="#">
-            <div class="service_card_wraper">
-              <div class="img_wraper">
-                <div class="overlay">
-                  <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <img src="assets/media/pic3.jpeg" alt="">
-              </div>
-
-              <div class="card_title_wraper">
-                <h4> Service 3 </h4>
-              </div>
-
-              <div class="card_text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
-                facilis. Quas ex provident quam, inventore consectetur 
-              </div>
-            </div>
-          </a>
-
-        </div>
-
-        <div class="col-10 col-lg-4 my-3">
-          <a href="#">
-            <div class="service_card_wraper">
-              <div class="img_wraper">
-                <div class="overlay">
-                  <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <img src="assets/media/pic4.jpeg" alt="">
-              </div>
-
-              <div class="card_title_wraper">
-                <h4> Service 4 </h4>
-              </div>
-
-              <div class="card_text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
-                facilis. Quas ex provident quam, inventore consectetur 
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-10 col-lg-4 my-3">
-          <a href="#">
-            <div class="service_card_wraper">
-              <div class="img_wraper">
-                <div class="overlay">
-                  <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <img src="assets/media/pic5.jpeg" alt="">
-              </div>
-
-              <div class="card_title_wraper">
-                <h4> Service 5 </h4>
-              </div>
-
-              <div class="card_text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
-                facilis. Quas ex provident quam, inventore consectetur 
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-10 col-lg-4 my-3">
-          <a href="#">
-            <div class="service_card_wraper">
-              <div class="img_wraper">
-                <div class="overlay">
-                  <i class="fas fa-info-circle fa-3x"></i>
-                </div>
-                <img src="assets/media/pic6.jpeg" alt="">
-              </div>
-
-              <div class="card_title_wraper">
-                <h4> Service 6 </h4>
-              </div>
-
-              <div class="card_text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
-                facilis. Quas ex provident quam, inventore consectetur 
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
+    <div class="sec_desc_wraper wow bounceIn" data-wow-duration="1.5s" data-wow-delay="0.9s">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+      Culpa enim praesentium minima sunt, eveniet ratione velit 
+      similique omnis porro doloremque dolores? Aut accusantium 
+      possimus voluptates nobis ullam? Amet, ipsa est.
     </div>
 
-    <div class="section_lower_curve_wraper">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
-      </svg>
+    <div class="row justify-content-center mt-5 wow bounceInLeft" data-wow-duration="1.5s" data-wow-delay="0.5s">
+      <div class="col-10 col-lg-4 my-5">
+        <a href="#">
+          <div class="service_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic1.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Service 1 </h4>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-5">
+        <a href="#">
+          <div class="service_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic2.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Service 2 </h4>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-5">
+        <a href="#">
+          <div class="service_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic3.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Service 3 </h4>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+
+      </div>
+
+      <div class="col-10 col-lg-4 my-5">
+        <a href="#">
+          <div class="service_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic4.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Service 4 </h4>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-5">
+        <a href="#">
+          <div class="service_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic5.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Service 5 </h4>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-10 col-lg-4 my-5">
+        <a href="#">
+          <div class="service_card_wraper">
+            <div class="img_wraper">
+              <div class="overlay">
+                <i class="fas fa-info-circle fa-3x"></i>
+              </div>
+              <img src="assets/media/pic6.jpeg" alt="">
+            </div>
+
+            <div class="card_title_wraper">
+              <h4> Service 6 </h4>
+            </div>
+
+            <div class="card_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Et corrupti minus ipsa aut, tenetur laboriosam nisi soluta 
+              facilis. Quas ex provident quam, inventore consectetur 
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </div>
