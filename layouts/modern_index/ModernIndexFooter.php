9310e3ac5848d73cace10d626efb001d568629f0
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-05-18 09:42:01 +0200

    finish modern index footer

--- layouts/modern_index/ModernIndexFooter.php
+++ layouts/modern_index/ModernIndexFooter.php
@@ -3,7 +3,7 @@
 
   <div class="section_curve">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-      <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
+      <path d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z" class="shape-fill"></path>
     </svg>
   </div>
 
