08a1b671424f3b16ffe86af0af35d9b157ab1147
MahmoudSiliman <modysiliman75@gmail.com>
2021-05-21 03:06:21 +0200

    finish mobile menu

--- layouts/ProfileHeader.php
+++ layouts/ProfileHeader.php
@@ -248,7 +248,7 @@
 <!-- END:: LARGE SCREENS NAVBAR -->
 
 <!-- START:: SMALL SCREENS MENU -->
-<!-- <div class="small_menu">
+<div class="small_menu">
   <div class="btn_box">
     <button id="colse_menu">
       <i class="far fa-times-circle"></i>
@@ -257,46 +257,188 @@
 
   <ul class="list-unstyled mb-0">
     <li class="active"> 
-      <a href="index.php"> Home </a>
+      <a href="javascript:;" class="dropdown_toggeler"> Home </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="index.php"> Home 1 </a> 
+        </li>
+
+        <li> 
+          <a href="index_2.php"> Home 2 </a> 
+        </li>
+
+        <li> 
+          <a href="index_one_page.php"> One Page </a> 
+        </li>
+
+        <li> 
+          <a href="index_modern.php"> Modern Home </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="AboutUs.php"> About Us </a>
+      <a href="javascript:;" class="dropdown_toggeler"> About Us </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="AboutUs1.php"> About Us 1 </a> 
+        </li>
+
+        <li> 
+          <a href="AboutUs2.php"> About Us 2 </a> 
+        </li>
+
+        <li> 
+          <a href="AboutModern.php"> About Us Modern </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="Faq.php"> FAQs </a>
+      <a href="javascript:;" class="dropdown_toggeler"> Services </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="AllServices1.php"> Services 1 </a> 
+        </li>
+
+        <li> 
+          <a href="AllServices2.php"> Services 2 </a> 
+        </li>
+
+        <li> 
+          <a href="AllServicesModern.php"> Services Modern </a> 
+        </li>
+
+        <li> 
+          <a href="SingleService1.php"> Single Service 1 </a> 
+        </li>
+
+        <li> 
+          <a href="SingleService2.php"> Single Service 2 </a>  
+        </li>
+
+        <li> 
+          <a href="SingleServiceModern.php"> Single Service Modern </a>  
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="AllProducts.php"> Product </a>
+      <a href="javascript:;" class="dropdown_toggeler"> FAQs </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="Faqs.php"> FAQs 1 </a>
+        </li>
+
+        <li> 
+          <a href="FaqsModern.php"> FAQs Modern </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="Quality.php"> Quality </a>
+      <a href="javascript:;" class="dropdown_toggeler"> Our Team </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="Team.php"> Our Team 1 </a>
+        </li>
+
+        <li> 
+          <a href="TeamModern.php"> Our Team Modern </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="Media.php"> Media </a>
+      <a href="javascript:;" class="dropdown_toggeler"> Portfolio </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="Portfolio.php"> Portfolio 1 </a>
+        </li>
+
+        <li> 
+          <a href="PortfolioModern.php"> Portfolio Modern </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="AllVideos.php"> Videos </a>
+      <a href="javascript:;" class="dropdown_toggeler"> Products </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="AllProducts1.php"> All Products 1 </a> 
+        </li>
+
+        <li> 
+          <a href="AllProducts2.php"> All Products 2 </a> 
+        </li>
+
+        <li> 
+          <a href="AllProductsModern.php"> All Products Modern </a> 
+        </li>
+
+        <li> 
+          <a href="SingleProduct1.php"> Single Product 1 </a> 
+        </li>
+
+        <li> 
+          <a href="SingleProduct2.php"> Single Product 2 </a> 
+        </li>
+
+        <li> 
+          <a href="SingleProductModern.php"> Single Product Modern </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="Carrers.php"> Carrers </a>
+      <a href="javascript:;" class="dropdown_toggeler"> Blog </a>
+
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="SidebarBlog.php"> Blog With SideBar </a> 
+        </li>
+
+        <li> 
+          <a href="BlogModern.php"> Blog Modern </a> 
+        </li>
+
+        <li> 
+          <a href="SidebarArticle.php"> Article With SideBar </a> 
+        </li>
+
+        <li> 
+          <a href="ModernArticle.php"> Article Modern </a> 
+        </li>
+      </ul>
     </li>
 
     <li> 
-      <a href="ContactUs.php"> Contact Us </a>
-    </li>
+      <a href="javascript:;" class="dropdown_toggeler"> Contact Us </a>
 
-    <li> 
-      <a href="UserProfile.php"> Profile </a>
+      <ul class="dropdown_menu">
+        <li> 
+          <a href="ContactUs1.php">Contact Us 1</a> 
+        </li>
+
+        <li> 
+          <a href="ContactUs2.php">Contact Us 2</a>  
+        </li>
+
+        <li> 
+          <a href="ContactUs3.php">Contact Us 3</a> 
+        </li>
+      </ul>
     </li>
   </ul>
-</div> -->
+</div>
 <!-- END:: SMALL SCREENS MENU -->
 
 <!-- START:: PRE LOADER -->
